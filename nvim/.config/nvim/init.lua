201a0bc28de707512eb7b6ef103f0485562c4813
stackcats <stackcats.hk@gmail.com>
2026-07-22 18:19:23 +0800

    fix(treesitter): add start cmd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,4 +6,10 @@ vim.loader.enable()
 require("stackcats.core")
 require("stackcats.lazy")
 
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
+})
+
 vim.cmd("colorscheme rose-pine")
